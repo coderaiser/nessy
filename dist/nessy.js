e60e3387165312a22f081f5ea80e4598a5786836
coderaiser <coderaiser@cloudcmd.io>
2016-09-16 12:23:07 +0300

    chore(package) v1.1.0

--- dist/nessy.js
+++ dist/nessy.js
@@ -9,9 +9,7 @@ module.exports = function (selector, value, obj) {
     check(selector);
 
     selector.split('.').forEach(function (name, i, arr) {
-        if (typeof obj[name] === 'undefined') {
-            if (i === arr.length - 1) obj[name] = value;else obj[name] = {};
-        }
+        if (i === arr.length - 1) obj[name] = value;else obj[name] = {};
 
         obj = obj[name];
     });
